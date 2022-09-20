7638947b40ec149d87fa1611baccabacc6fdc765
LocNguyen <nguye2lc@mail.uc.edu>
2022-09-20 12:37:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age;
+        System.out.println("What is your age ?");
+        // Stimulate the input code which we learn later
+        age = 63;
+
+        if (age < 15)
+        {
+            System.out.println("You can enter the Children's Cave");
+
+        }
+    }
+}
